ad30d0eecd64d07acc03ca7ef8111621b2371793
Delfa Fernandes <fernandesdelfa1234@gmail.com>
2024-02-12 09:28:13 +0530

    second

--- Casetool_ISA1_set8/src/Exam/AsciiValue.java
+++ Casetool_ISA1_set8/src/Exam/AsciiValue.java
@@ -12,5 +12,18 @@ public class AsciiValue {
 		System.out.println("The ASCII value of "+ ch + "is :"+ castAscii);
 
 	}
+	public  class VowelConsonant{
+		public void main(String[] args) {
+			char ch='i';
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+				System.out.println(ch +" is vowel");
+			else
+				System.out.println(ch +" is consonant" );
+			
+		}
+	}
 
 }
+
+
+
